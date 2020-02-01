566f1d5efeee253b306d328ccbb29afd7935dd86
Igor Sudenko <sudenkoweb@gmail.com>
2020-02-01 16:44:32 +0200

    index.js: почистил от хлама;

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import SwapiService from "./services/swapi-service";
-
-const swapiService = new SwapiService();
-
-swapiService.fetchPerson(1)
-  .then((body) => {
-    console.log(body);
-  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
