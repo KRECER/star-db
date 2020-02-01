aeb467e5b221998b2eb0eedeca81717edd589bc8
Igor Sudenko <sudenkoweb@gmail.com>
2020-02-01 19:14:34 +0200

    component/random-planet.js: Добавил логику прелоадера;

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './random-planet.scss';
 import SwapiService from "../../services/swapi-service";
+import Preloader from "../preloader";
 
 class RandomPlanet extends Component {
 
   swapiService = new SwapiService();
 
   state = {
-    planet: {}
+    planet: {},
+    loading: true
   };
 
   planetLoadedHandler = (planet) => {
-    this.setState({planet});
+    this.setState({planet, loading: false});
   };
 
   updatePlanet() {
@@ -24,35 +26,50 @@ class RandomPlanet extends Component {
 
   render() {
 
-    const {planet: {id, name, population, rotationPeriod, diameter}} = this.state;
+    const {planet, loading} = this.state;
+    const spinner = loading ? <Preloader/> : null;
+    const content = !loading ? <PlanetView planet={planet}/> : null;
 
     return (
       <section className="random-planet">
         <div className="random-planet__inner">
-          <div className="random-planet__image-wrap">
-            <img className="random-planet__image" src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="" width="225" height="255"/>
-          </div>
-          <div className="random-planet__content">
-            <h2 className="random-planet__title">{name}</h2>
-            <ul className="random-planet__list site-list">
-              <li className="random-planet__item">
-                <span className="random-planet__item-name">Population</span>
-                <span className="random-planet__item-value">{population}</span>
-              </li>
-              <li className="random-planet__item">
-                <span className="random-planet__item-name">Rotation&nbsp;Period</span>
-                <span className="random-planet__item-value">{rotationPeriod}</span>
-              </li>
-              <li className="random-planet__item">
-                <span className="random-planet__item-name">Diameter</span>
-                <span className="random-planet__item-value">{diameter}</span>
-              </li>
-            </ul>
-          </div>
+          {spinner}
+          {content}
         </div>
       </section>
     );
   }
 }
+
+const PlanetView = ({planet}) => {
+  const {id, name, population, rotationPeriod, diameter} = planet;
+
+  return (
+    <React.Fragment>
+      <div className="random-planet__image-wrap">
+        <img className="random-planet__image" src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+             alt="" width="225" height="255"/>
+      </div>
+      <div className="random-planet__content">
+        <h2 className="random-planet__title">{name}</h2>
+        <ul className="random-planet__list site-list">
+          <li className="random-planet__item">
+            <span className="random-planet__item-name">Population</span>
+            <span className="random-planet__item-value">{population}</span>
+          </li>
+          <li className="random-planet__item">
+            <span className="random-planet__item-name">Rotation&nbsp;Period</span>
+            <span className="random-planet__item-value">{rotationPeriod}</span>
+          </li>
+          <li className="random-planet__item">
+            <span className="random-planet__item-name">Diameter</span>
+            <span className="random-planet__item-value">{diameter}</span>
+          </li>
+        </ul>
+      </div>
+
+    </React.Fragment>
+  );
+};
 
 export default RandomPlanet;
